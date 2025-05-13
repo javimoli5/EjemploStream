c5636d0e01b6986777278b247f2f32753534eee4
javier molina <xaviermpmm@gmail.com>
2025-05-13 12:38:29 +0200

    Añado modificacion javadoc de FUtil

--- src/es/maestredam/FUtil.java
+++ src/es/maestredam/FUtil.java
@@ -2,6 +2,12 @@ package es.maestredam;
 import java.io.File;
 
 public class FUtil {
+	
+	/**
+	 * Metodo que verifica si una cadena pasada existe o no
+	 * @param filename cadena o fichero pasado
+	 * @return true si existe, false si no; si es una cadena vacia lanza una excepcion
+	 */
 	public static boolean existe(String filename) {
 		File file = new File(filename);
 		if (filename.isBlank()) {
